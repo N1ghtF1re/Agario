41736316a2148532836114ebe5f36a0bade30e7e
Alexandr Pankratiew <sasha.pankratiew@gmail.com>
2019-02-23 21:47:44 +0300

    Added mobs

--- server/src/main/java/men/brakh/agario/model/communicator/EmptyCommunicator.java
+++
@@ -1,8 +0,0 @@
-package men.brakh.agario.model.communicator;
-
-import men.brakh.agario.model.message.Message;
-
-public class EmptyCommunicator implements Communicator {
-    @Override
-    public void send(Message message) {}
-}
